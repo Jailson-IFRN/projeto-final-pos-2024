8cfa589918b6cbc69fa89f5980af17f88510a097
Maria Rita <mariaritastrn@gmail.com>
2024-12-28 14:59:27 -0300

    parte 01- projeto

---
+++ placeholder_api/api/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import UserViewSet, PostViewSet, CommentViewSet, AlbumViewSet, PhotoViewSet, ToDoViewSet
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'posts', PostViewSet)
+router.register(r'comments', CommentViewSet)
+router.register(r'albums', AlbumViewSet)
+router.register(r'photos', PhotoViewSet)
+router.register(r'todos', ToDoViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
