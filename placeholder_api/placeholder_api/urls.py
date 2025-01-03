5a4644c3f2b5ef6bd166a01a737081ab3bf1362f
Jailson <jg094151@gmail.com>
2025-01-03 14:19:52 -0300

    Reconfigurando - tirando axios, usando fecth, alinhando modelos no apiWrapper e configurando o App.js

--- placeholder_api/placeholder_api/urls.py
+++ placeholder_api/placeholder_api/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 from django.http import HttpResponseRedirect
 
 urlpatterns = [
-    path('', lambda request: HttpResponseRedirect('/api/')),  
+    path('', lambda request: HttpResponseRedirect('/api/')),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
